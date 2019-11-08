6ec61d38b9c6d707074cfbbb62a31b6dfabab7b8
karl thomas <k.bonnitto@gmail.com>
2019-11-07 20:03:31 -0500

    added register and layout views, updated application py and created user table

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 import os
 
 from flask import Flask, render_template, session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, Boolean
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+# its dangerous package to handle passwords and such
+from itsdangerous import URLSafeTimedSerializer
 
 from flask_session import Session
 
@@ -18,10 +21,21 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
 db = scoped_session(sessionmaker(bind=engine))
 
+metadata = MetaData()
 
+users = Table('users', metadata,
+              Column('id', Integer, primary_key=True),
+              Column('username', String, nullable=False, unique=True),
+              Column('password', String, nullable=False),
+              Column('first_name', String(20)),
+              Column('last_name', String(20)),
+              Column('avatar_url', String))
+
+
+metadata.create_all(engine)
 
 # API key and secret
 api_key = "m2yuVbMZPhY2Es41AhbA"
@@ -31,6 +45,10 @@ secret = "uZux5gULwA97zc9cDRPkD4tS4yj0l7ZHJKOc5B05Fk"
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ models/User.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, username, password, first_name, last_name, avatar_url):
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.avatar_url = avatar_url
